3e4e0a85bb242d85b6afb32afc6082c5e613a13d
soj132 <sam@luckyegg.co>
2026-05-13 10:40:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # --- Config ---
-APP_ID = "cli_aa8e8364f563de15"
-APP_SECRET = "mt8PFUb75aCh8jq993Lblcxw8NHki1Do"
-VERIFICATION_TOKEN = "gLmv9matJzhWQRCSnUrgZee4pkwrOd1J
-"
+import os
+
+APP_ID = os.environ.get("APP_ID")
+APP_SECRET = os.environ.get("APP_SECRET")
+VERIFICATION_TOKEN = os.environ.get("VERIFICATION_TOKEN")
 
 def get_tenant_access_token():
     url = "https://open.larksuite.com/open-apis/auth/v3/tenant_access_token/internal"
